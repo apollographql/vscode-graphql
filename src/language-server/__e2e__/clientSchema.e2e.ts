91d511a9eaf010b796ffe4ed37f2f11cdfbcd3a7
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-07-29 10:23:30 +0200

    test comments

--- src/language-server/__e2e__/clientSchema.e2e.ts
+++ src/language-server/__e2e__/clientSchema.e2e.ts
@@ -8,12 +8,16 @@ beforeAll(async () => {
 });
 
 test("completion", async () => {
+  // dro|id
   await testCompletion(editor, [4, 7], [["droid", "Droid"]]);
+  // na|me
   await testCompletion(editor, [5, 8], [["name", "String!"]]);
+  // mo|del
   await testCompletion(editor, [6, 8], [["model", "String"]]);
 });
 
 test("hover", async () => {
+  // featu|reFlagDefer
   expect(await getHover(editor, [3, 10])).toMatchInlineSnapshot(`
 "\`\`\`graphql
 Query.featureFlagDefer: Boolean!
@@ -28,6 +32,7 @@ Query.featureFlagDefer: Boolean!
 Whether to use defer"
 `);
 
+  // @c|lient(always: false)
   expect(await getHover(editor, [3, 24])).toMatchInlineSnapshot(`
 "\`\`\`graphql
 @client(always: Boolean)
@@ -38,6 +43,7 @@ Whether to use defer"
 Direct the client to resolve this field locally, either from the cache or local resolvers."
 `);
 
+  // @client(alwa|ys: false)
   expect(await getHover(editor, [3, 33])).toMatchInlineSnapshot(`
 "\`\`\`graphql
 always: Boolean
@@ -49,6 +55,7 @@ When true, the client will never use the cache for this value. See
 https://www.apollographql.com/docs/react/local-state/local-resolvers/#forcing-resolvers-with-clientalways-true"
 `);
 
+  // @expo|rt(as: "defer")
   expect(await getHover(editor, [3, 49])).toMatchInlineSnapshot(`
 "\`\`\`graphql
 @export(as: String!)
@@ -69,6 +76,7 @@ as: String!
 The variable name to export this field as."
 `);
 
+  // @nonre|active
   expect(await getHover(editor, [7, 28])).toMatchInlineSnapshot(`
 "\`\`\`graphql
 @nonreactive
@@ -81,6 +89,7 @@ This allows parent components to fetch data to be rendered by their children wit
 https://www.apollographql.com/docs/react/data/directives#nonreactive"
 `);
 
+  // @def|er(if: $defer, label: "fc")
   expect(await getHover(editor, [8, 14])).toMatchInlineSnapshot(`
 "\`\`\`graphql
 @defer(if: Boolean, label: String)
@@ -92,6 +101,7 @@ This directive enables your queries to receive data for specific fields incremen
 This is helpful whenever some fields in a query take much longer to resolve than others.
 https://www.apollographql.com/docs/react/data/directives#defer"
 `);
+  //@defer(i|f: $defer, label: "fc")
   expect(await getHover(editor, [8, 18])).toMatchInlineSnapshot(`
 "\`\`\`graphql
 if: Boolean
@@ -101,6 +111,7 @@ if: Boolean
 
 When true fragment may be deferred, if omitted defaults to true."
 `);
+  //@defer(if: $defer, labe|l: "fc")
   expect(await getHover(editor, [8, 33])).toMatchInlineSnapshot(`
 "\`\`\`graphql
 label: String
@@ -112,7 +123,7 @@ A unique label across all @defer and @stream directives in an operation.
 This label should be used by GraphQL clients to identify the data from patch responses and associate it with the correct fragment.
 If provided, the GraphQL Server must add it to the payload."
 `);
-
+  // @connec|tion(key: "feed")
   expect(await getHover(editor, [9, 53])).toMatchInlineSnapshot(`
 "\`\`\`graphql
 @connection(key: String!, filter: [String!])
@@ -123,6 +134,7 @@ If provided, the GraphQL Server must add it to the payload."
 Specify a custom store key for this result. See
 https://www.apollographql.com/docs/react/caching/advanced-topics/#the-connection-directive"
 `);
+  // @connection(ke|y: "feed")
   expect(await getHover(editor, [9, 61])).toMatchInlineSnapshot(`
 "\`\`\`graphql
 key: String!

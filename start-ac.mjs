f32b841af29bf3c86ba80c8832eb48f8c578a044
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-08-30 12:54:46 +0200

    testing harness

--- start-ac.mjs
+++ start-ac.mjs
@@ -1,3 +1,8 @@
+#!/usr/bin/env node
+
+// for testing, start a few of these, e.g. with
+// while true; do echo "foo\nbar\nbaz" | parallel ./start-ac.mjs; sleep 1; done
+
 import { registerClient } from "./devtool-build/vscode-client.js";
 import WebSocket from "ws";
 import { ApolloClient, InMemoryCache } from "@apollo/client/core/index.js";
@@ -16,12 +21,24 @@ const link = new MockLink([
   {
     request: { query: helloWorld },
     result: { data: { hello: "world" } },
+    maxUsageCount: 1000,
+  },
+  {
+    request: {
+      query: gql`
+        query {
+          hi
+        }
+      `,
+    },
+    result: { data: { hi: "universe" } },
+    maxUsageCount: 1000,
   },
 ]);
 const client = new ApolloClient({
   link,
   cache: new InMemoryCache(),
+  devtools: { name: process.argv[2] },
 });
 client.watchQuery({ query: helloWorld }).subscribe({ next() {} });
-
 registerClient(client, "ws://localhost:8090");

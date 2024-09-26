cc355ecf4afa6b8d12da13d2551c83bc8366324b
Dylan Anthony <dylan@apollographql.com>
2024-09-26 11:35:23 -0600

    Auto-enable debug-level logging

--- src/language-server/project/rover/project.ts
+++ src/language-server/project/rover/project.ts
@@ -179,6 +179,9 @@ export class RoverProject extends GraphQLProject {
     if (this.config.rover.supergraphConfig) {
       args.push("--supergraph-config", this.config.rover.supergraphConfig);
     }
+    if (Debug.traceLevel >= TraceLevel.verbose) {
+      args.push("--log", "debug");
+    }
     args.push(...this.config.rover.extraArgs);
 
     Debug.traceVerbose(

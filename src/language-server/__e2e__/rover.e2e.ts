12a59fd647e42c760bbe795acb355b412bd424ec
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2025-03-19 14:21:04 -0700

    remove profile check in rover test, it’s not needed

--- src/language-server/__e2e__/rover.e2e.ts
+++ src/language-server/__e2e__/rover.e2e.ts
@@ -16,31 +16,7 @@ import {
   getDefinitions,
 } from "./utils";
 
-// we want to skip these tests unless the user running them has a rover config profile named "VSCode-E2E"
 let test = origTest.skip;
-try {
-  const roverProjectDir = join(__dirname, "../../../sampleWorkspace/rover");
-  const config = load(
-    readFileSync(join(roverProjectDir, "apollo.config.yaml"), "utf-8"),
-  ) as ParsedApolloConfigFormat;
-  const roverBin = join(roverProjectDir, config.rover!.bin);
-  const result = execFileSync(roverBin, [
-    "config",
-    "list",
-    "--format=json",
-  ]).toString("utf8");
-  const parsed = JSON.parse(result);
-  if (parsed.data.profiles.includes("VSCode-E2E")) {
-    test = origTest;
-  }
-} catch (e) {}
-if (test === origTest.skip) {
-  console.info(
-    "Skipping rover E2E tests: no profile with the name 'VSCode-E2E'\n" +
-      "You can create one by running `rover config auth --profile VSCode-E2E`",
-  );
-}
-
 if (process.platform === "win32") {
   console.info("Skipping rover E2E tests in Windows");
   test = origTest.skip;
